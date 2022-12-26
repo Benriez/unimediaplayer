ad68ec13e0aec866c7d9e8efc004507685324d88
Benriez <stberiez@stud.h-da.de>
2022-12-26 20:32:06 +0100

    refactor queuecard component

--- src/components/QueueCard.vue
+++ src/components/QueueCard.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="row" style="width:100%; height: 5rem; background-color: white">
-    <div class="col-2" style="height: 100%; padding: .5rem;">
-      <div class="column" style="display: flex; justify-content: center; width:100%; height: 100%; background-color: black; border-radius: .25rem;">
-        <q-icon class="self-center" name="play_arrow" style="font-size: 1.5rem; color: white;"/>
+  <div class="row queue-card">
+    <div class="col-2 queue-card-left">
+      <div class="column queue-card-preview">
+        <q-icon class="self-center q-icon" name="play_arrow" style="color: white;"/>
       </div>
     </div>
-    <div class="col-9" style="padding: .3rem; height:100%; border-left: 1px solid rgb(0 0 0 / 18%);border-right: 1px solid rgb(0 0 0 / 18%)">
-      <p style="margin-bottom: 0; margin-left: 1rem;">Trackname</p>
-      <p style="margin-bottom: 0; margin-left: 1rem;">Album</p>
-      <p style="margin-bottom: 0; margin-left: 1rem;">Artist</p>
 
+
+    <div class="col-9 queue-card-center">
+      <p class="queue-card-text">Trackname</p>
+      <p class="queue-card-text">Album</p>
+      <p class="queue-card-text">Artist</p>
     </div>
-    <div class="col-1 column" style="height:100%; display: flex; justify-content: center;">
-      <q-icon class="self-center" name="drag_indicator" style="font-size: 1.5rem;"/>
+
+
+    <div class="col-1 queue-card-right column">
+      <q-icon class="self-center q-icon" name="drag_indicator" />
     </div>
   </div>
 </template>
@@ -25,3 +28,48 @@ export default defineComponent({
 
 })
 </script>
+
+
+<style>
+  .queue-card{
+    width:100%; 
+    height: 5rem; 
+    background-color: white
+  }
+
+  .queue-card-left{
+    height: 100%; 
+    padding: .5rem;
+  }
+
+  .queue-card-center{
+    padding: .3rem; 
+    height:100%; 
+    border-left: 1px solid rgb(0 0 0 / 18%);
+    border-right: 1px solid rgb(0 0 0 / 18%)
+  }
+
+  .queue-card-right{
+    height:100%; 
+    display: flex; 
+    justify-content: center;
+  }
+
+  .queue-card-preview{
+    display: flex; 
+    justify-content: center; 
+    width:100%; 
+    height: 100%; 
+    background-color: black; 
+    border-radius: .25rem;
+  }
+
+  .queue-card-text{
+    margin-bottom: 0; 
+    margin-left: 1rem;
+  }
+
+  .q-icon{
+    font-size: 1.5rem;
+  }
+</style>
