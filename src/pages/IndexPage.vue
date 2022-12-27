7dd720a934221b78912b57a7691f77365855b445
Benriez <stberiez@stud.h-da.de>
2022-12-27 02:10:11 +0100

    update css

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -13,7 +13,7 @@
           <!-- https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/344277146&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true -->
           <!-- <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/344277146&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/bendust_official" title="Ben Dust" target="_blank" style="color: #cccccc; text-decoration: none;">Ben Dust</a> · <a href="https://soundcloud.com/bendust_official/bentech-no-one-takes-me-down-ben-dust-remix-preview" title="Bentech &amp; Sis - No One Takes Me Down (Ben Dust Remix) - OUT NOW" target="_blank" style="color: #cccccc; text-decoration: none;">Bentech &amp; Sis - No One Takes Me Down (Ben Dust Remix) - OUT NOW</a></div> -->
           <div class="queue" style="width: 100%; height: 100%;">
-            <div style="background-color:white; border-bottom: 1px solid rgb(0 0 0 / 18%);display:flex;display: flex; justify-content: space-between; padding: .5rem">
+            <div class="queue-wrapper">
               <p style="margin-bottom: 0">Queue ( / {{queueList.length}})</p>
               <q-btn label="Add" @click="addLink" class="addBtn">
                 <q-icon class="self-center q-icon" name="add_circle" style="margin-left:.5rem" />
@@ -45,15 +45,29 @@ import TweenMax from 'gsap'
 
 const trackData = [
   {
-    trackname: 'Awesometrack',
-    album: 'the dudes',
-    artist: 'psychonaut'
+    trackname: 'The Crusaders - My Lady (Todd Terje Tangoterje Edit)',
+    album: '',
+    artist: 'dj tigerstripes',
+    url: 'https://soundcloud.com/dj-tiger-stripes/the-crusaders-my-lady-todd-terje-tangoterje-edit'
   },
   {
-    trackname: 'Track 2',
-    album: 'the dudes',
-    artist: 'psychonaut'
+    trackname: 'Diamond Veins feat. Sarah Rebecca',
+    album: 'album',
+    artist: 'French79',
+    url: 'https://soundcloud.com/french79music/diamond-veins'
   },
+  {
+    trackname: 'Inside My Love',
+    album: 'album',
+    artist: 'Buzz Compass',
+    url: 'https://soundcloud.com/buzzcompass/inside-my-love-rework'
+  },
+  {
+    trackname: 'Session #8 Lydia Birthday',
+    album: 'album',
+    artist: 'Psychonaut25',
+    url: 'https://livesets.com/psychonaut25/session/54483'
+  }
 ]
 
 
@@ -100,5 +114,13 @@ export default defineComponent({
 <style>
   .addBtn:before{
     box-shadow: none;
+  }
+
+  .queue-wrapper{
+    background-color:white; 
+    border-bottom: 1px solid rgb(0 0 0 / 18%);
+    display:flex;display: flex; 
+    justify-content: space-between; 
+    padding: .5rem
   }
 </style>
