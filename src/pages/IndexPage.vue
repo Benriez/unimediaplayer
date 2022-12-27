07de6d23abc07175ffc028976d45aceaf64a6bff
Benriez <stberiez@stud.h-da.de>
2022-12-27 01:37:05 +0100

    add queuelist counter

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -14,8 +14,8 @@
           <!-- <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/344277146&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/bendust_official" title="Ben Dust" target="_blank" style="color: #cccccc; text-decoration: none;">Ben Dust</a> · <a href="https://soundcloud.com/bendust_official/bentech-no-one-takes-me-down-ben-dust-remix-preview" title="Bentech &amp; Sis - No One Takes Me Down (Ben Dust Remix) - OUT NOW" target="_blank" style="color: #cccccc; text-decoration: none;">Bentech &amp; Sis - No One Takes Me Down (Ben Dust Remix) - OUT NOW</a></div> -->
           <div class="queue" style="width: 100%; height: 100%;">
             <div style="background-color:white; border-bottom: 1px solid rgb(0 0 0 / 18%);display:flex;display: flex; justify-content: space-between; padding: .5rem">
-              <p style="margin-bottom: 0">Queue</p>
-              <q-btn label="Add" @click="addLink">
+              <p style="margin-bottom: 0">Queue ( / {{queueList.length}})</p>
+              <q-btn label="Add" @click="addLink" class="addBtn">
                 <q-icon class="self-center q-icon" name="add_circle" style="margin-left:.5rem" />
               </q-btn>
             </div>
@@ -95,3 +95,10 @@ export default defineComponent({
   
 })
 </script>
+
+
+<style>
+  .addBtn:before{
+    box-shadow: none;
+  }
+</style>
